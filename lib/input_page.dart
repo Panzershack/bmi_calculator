dd4f0436056dcadb7a0d5bc43c749d98790705c5
Panzershack <pasannim@yahoo.com>
2024-02-10 10:20:08 +0530

    created expanded containers to accomodate UI components, created an extracted widgetClass to reuse them in the app

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -9,20 +9,70 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'BMI CALCULATOR',
-          style: TextStyle(
-            color: Colors.white,
+        appBar: AppBar(
+          title: Text(
+            'BMI CALCULATOR',
+            style: TextStyle(
+              color: Colors.white,
+            ),
           ),
         ),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: reusableCard(
+                      Color(0xFF1d1E33),
+                    ),
+                  ),
+                  Expanded(
+                    child: reusableCard(
+                      Color(0xFF1d1E33),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: reusableCard(
+                Color(0xFF1d1E33),
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: reusableCard(
+                      Color(0xFF1d1E33),
+                    ),
+                  ),
+                  Expanded(
+                    child: reusableCard(
+                      Color(0xFF1d1E33),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class reusableCard extends StatelessWidget {
+  reusableCard(this.myColor);
+
+  final Color myColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: myColor,
       ),
     );
   }
