c6b3c5c4d2e703aaef2d14411c0b7f7361a0790d
Panzershack <pasannim@yahoo.com>
2024-02-11 16:33:41 +0530

    requires some fixing on the overloaded constructor

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const Color darkBlue = Color(0xFF1d1E33);
@@ -28,8 +29,25 @@ class _InputPageState extends State<InputPage> {
               children: [
                 Expanded(
                   child: reusableCard(
-                    darkBlue,
-                  ),
+                      darkBlue,
+                      Column(
+                        children: [
+                          Icon(
+                            FontAwesomeIcons.mars,
+                            size: 80.0,
+                          ),
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                          Text(
+                            'MALE',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              color: Color(0xFF8D8E98),
+                            ),
+                          )
+                        ],
+                      )),
                 ),
                 Expanded(
                   child: reusableCard(
@@ -72,14 +90,23 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+const Widget sizedbox = SizedBox();
+
 class reusableCard extends StatelessWidget {
-  reusableCard(this.myColor);
+  reusableCard(
+    this.myColor,
+    this.cardChild,
+  );
+
+  reusableCard.withColor(this.myColor, {this.cardChild = sizedbox});
 
   final Color myColor;
+  final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10.0),
